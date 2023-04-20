dfc3a39a18b71b0c46a53d989f41a78b51275784
royalcoder99 <royalcoder99@gmail.com>
2023-04-20 13:54:27 +0530

    up

--- session.php
+++ session.php
@@ -13,5 +13,17 @@ $_SESSION["favanimal"] = "cat";
 echo "Session variables are set.";
 ?>
 
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Echo session variables that were set on previous page
+echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
+echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
+?>
 </body>
 </html>
