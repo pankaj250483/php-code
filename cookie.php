d51f0bedceb65ff6ffba25fa8d41af9a151228ec
royalcoder99 <royalcoder99@gmail.com>
2023-04-20 16:13:32 +0530

    up

--- cookie.php
+++ cookie.php
@@ -15,30 +15,6 @@ if(!isset($_COOKIE[$cookie_name])) {
 }
 ?>
 
-<?php
-setcookie("test_cookie", "test", time() + 3600, '/');
-?>
-<html>
-<body>
-
-<?php
-if(count($_COOKIE) > 0) {
-  echo "Cookies are enabled.";
-} else {
-  echo "Cookies are disabled.";
-}
-?>
-
-<?php
-// set the expiration date to one hour ago
-setcookie("user", "", time() - 3600);
-?>
-<html>
-<body>
-
-<?php
-echo "Cookie 'user' is deleted.";
-?>
 
 </body>
 </html>
--- session.php
+++ session.php
@@ -13,17 +13,5 @@ $_SESSION["favanimal"] = "cat";
 echo "Session variables are set.";
 ?>
 
-<?php
-session_start();
-?>
-<!DOCTYPE html>
-<html>
-<body>
-
-<?php
-// Echo session variables that were set on previous page
-echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
-echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
-?>
 </body>
 </html>
