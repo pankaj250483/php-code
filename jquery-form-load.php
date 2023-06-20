287e548d027501bcc1e60734cdf8cb2fc81590bf
Pankaj Ojha <84920941+pankaj250483@users.noreply.github.com>
2023-06-20 15:05:07 +0530

    up

---
+++ jquery-form-load.php
@@ -0,0 +1,16 @@
+<?php
+
+// $x["st_name"]="pankaj";
+// $x["st_age"]=35;
+
+// echo json_encode($x);
+
+
+echo '[
+        {"st_name":"pankaj","st_age":35},
+        {"st_name":"dheeraj","st_age":22},
+        {"st_name":"geeta","st_age":18},
+        {"st_name":"sumit","st_age":28}
+        ]';
+
+?>
