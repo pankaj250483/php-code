6fecfd74c90072d9efe9304a3c264d2730cf75a6
Dheerajkumar2929 <136156010+Dheerajkumar2929@users.noreply.github.com>
2023-06-21 13:08:44 +0530

    u

--- jquery-form-load.php
+++ jquery-form-load.php
@@ -10,8 +10,9 @@ echo '[
         {"st_name":"pankaj","st_age":35},
         {"st_name":"dheeraj","st_age":22},
         {"st_name":"karun","st_age":18},
-        {"st_name":"sumit","st_age":28}
-        {"st_name":"geet","st_age":56}
+        {"st_name":"sumit","st_age":28},
+        {"st_name":"geet","st_age":56},
+        {"st_name":"ajay","st_age":34}
         ]';
 
 ?>
