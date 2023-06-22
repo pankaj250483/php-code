d71ac099754204e2e6e352f1e947414d76a480ac
Dheerajkumar2929 <136156010+Dheerajkumar2929@users.noreply.github.com>
2023-06-22 16:42:05 +0530

    up

--- jquery-form.php
+++ jquery-form.php
@@ -1,6 +1,36 @@
 <?php
-echo "PHP Script Run in Background succesffuly !<br>";
-echo "We have recevied your data as follows<br>name - ".$_POST["st_name"];
-echo "<br>age - ".$_POST["st_age"];
+
+echo ="run";
+return;
+  require "connect.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
+  
+ 
+  
+
+    $id=$_POST["st id"];
+    $name=$_POST["st_name"];
+    $english=$_POST["st_english"];
+    $math=$_POST["st_math"];
+    $hindi=$_POST["st_hindi"];
+    $science=$_POST["st_science"];
+    $punjabi=$_POST["st_punjabi"];
+    $gk=$_POST["st_gk"];
+    // var_dump($name,$city,$mobile);
+    
+    $sql="INSERT INTO `computers` ( `id`,`name`,`english`,`math`,`hindi`,`science`,`punjabi`,`gk`) VALUES ('$id','$name','$english','$math','$hindi','$science','$punjabi','$gk')";
+    
+    if ($conn->query($sql) === TRUE) {
+        $last_id = $conn->insert_id;
+        $message="<div class='alert alert-success'>New record created successfully. Last inserted ID is: " . $last_id."</div>";
+      } else 
+      {
+        $message="<div class='alert alert-danger'>Error: <br>" . $conn->error."</div>";
+      }
+      
+      $conn->close();
+}
 
 ?>
