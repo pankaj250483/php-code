40f2d52ef908cb2f641cd1d59212fe01355ab1ba
Pankaj Ojha <84920941+pankaj250483@users.noreply.github.com>
2023-07-13 15:44:52 +0530

    update

--- hello.php
+++ hello.php
@@ -4,12 +4,22 @@ require 'vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\Color;
 
-$spreadsheet = new Spreadsheet();
-$activeWorksheet = $spreadsheet->getActiveSheet();
-$activeWorksheet->setCellValue('A1', 'Hello World !');
+$x = new Spreadsheet();
+$w = $x->getActiveSheet();
+$w->setCellValue('A1', 'Hello PANKAJ !');
 
-$writer = new Xlsx($spreadsheet);
+$x
+    ->getActiveSheet()
+    ->getStyle('A1:A5')
+    ->getBorders()
+    ->getInside()
+    ->setBorderStyle(Border::BORDER_THICK)
+    ->setColor(new Color('FFFF0000'));
+
+$writer = new Xlsx($x);
 $writer->save('hello world.xlsx');
 
 ?>
