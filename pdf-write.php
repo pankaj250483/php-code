f602ecb94a2029421090ab948f0c05dcae398f44
Dheerajkumar2929 <136156010+Dheerajkumar2929@users.noreply.github.com>
2023-07-15 17:05:24 +0530

    up

--- pdf-write.php
+++ pdf-write.php
@@ -5,7 +5,29 @@ require_once('vendor/fpdf/html_table.php');
 ob_end_clean();
 
 $h="<h1>pankaj computers</h1>";
-$t="<p>This is message<p><table><tr><td>hello</td><td>hello 2</td></tr></table>";
+$t="<p>STUDENT MARKSHEET<p>
+<table>
+<tr>
+<td>Rollno</td>
+<td>Name</td>
+<td>english</td>
+<td>math</td>
+<td>hindi</td>
+<td>science</td>
+<td>punjabi</td>
+<td>gk</td>
+</tr>
+<tr>
+         <td>1</td>
+        <td>sonu</td>
+        <td>55</td>
+        <td>45</td>
+        <td>80</td>
+        <td>76</td>
+        <td>58</td>
+        <td>54</td>
+      </tr>
+</table>";
 
 $pdf=new PDF_HTML_Table();
 $pdf->AddPage();
